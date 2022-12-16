7b7764c9f8073578745b42361721597b6c9de8fa
hyejuc <107419210+hyejuc@users.noreply.github.com>
2022-12-17 01:39:56 +0900

    feat: TodoApp 추가

---
+++ src/main/java/com/test/TodoApp/TodoAppController.java
@@ -0,0 +1,12 @@
+package com.test.TodoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoAppController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application !";
+    }
+}
